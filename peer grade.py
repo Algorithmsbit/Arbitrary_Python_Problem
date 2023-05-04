2a7d63a853a447601f3a6381c26abf2c79a91036
Vrashali Kale <125857387+Algorithmsbit@users.noreply.github.com>
2023-05-04 14:15:05 +0530

    Add files via upload

---
+++ peer grade.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Peer Graded assignment
+
+# list of Data Science Languages
+# Python
+# Sql
+# R
+# JavaScript
+# Java
+# C
+# C++
+# Scala
+# Julia
+
+# Data Science Libraries
+# numpy
+# scipy
+# pandas
+# Sklearn
+
+# Data science tools:
+# Pandas 
+# Tableau 
+# Saas
+# SQL server
+# Tesorflow
+
+# a=19
+# b=45
+# print(a)
+# print(b)
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a%b)
+
+# In[ ]:
+
+
+a=19
+b=45
+print(a)
+print(b)
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+
+
+# In[ ]:
+
+
+T_min = 400
+h =T_min/60
+print(h)
+
+
+# list of objectives
+# Python list is a data structure that stores a collection of values or objects in a specified order (or sequence). 
+# Some features of Python lists include: They are mutable, which means that they can be changed or updated.
+# Python lists can store different types of objects and different data types
+
+# Authors name:
+# Vrashali K.
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
